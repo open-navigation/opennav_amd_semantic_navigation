499c07c9c99c1462e5ed3a8f5b86e123a16bc117
SteveMacenski <stevenmacenski@gmail.com>
2026-05-26 13:30:04 -0700

    added example gif
    Signed-off-by: SteveMacenski <stevenmacenski@gmail.com>

--- example/label_images.py
+++ example/label_images.py
@@ -18,16 +18,14 @@ import torch
 
 # Filename -> list of text prompts to segment with.
 IMAGE_PROMPTS = {
-    "cables.jpeg":                 ["Cables and chargers along the floor"], # TODO try small objects on the ground
-    "cables2.jpg":                 ["cables on the floor"], # TODO try small objects on the ground
-    "forklift_ground_forks.jpg":   ["forklift forks", "ground or floor"],
-    "glass_wall.jpg":              ["glass wall or door", "carpet", "wood, concrete, or tile floor"],
-    "lego.jpg":                    ["lego or legos", "carpet"], # TODO try small objects on the ground
-    "overhang_sign.jpg":           ["hanging or overhanging sign", "shelves", "floor or walkway"],
-    "pothole.jpg":                 ["pothole", "road"],
-    "puddle.png":                  ["puddle or water", "road"],
-    "tall_grass_push_through.jpg": ["tall grass a robot can drive over"],
-    "shovel.jpg":                  ["tools on the ground", "hole", "pile of dirt", "navigable flat ground"], #TODO last one
+    "cables.jpeg":                 ["Cables and chargers along the floor"], # OK
+    "forklift_ground_forks.jpg":   ["forklift forks", "ground or floor"], # OK
+    "glass_wall.jpg":              ["glass wall or door", "carpet", "wood, concrete, or tile floor"], # OK
+    "overhang_sign.jpg":           ["hanging sign", "floor or walkway"], #OK
+    "puddle.png":                  ["puddle or water", "road"], # OK
+    "tall_grass_push_through.jpg": ["tall grass a robot can drive over"], #OK
+    "shovel.jpg":                  ["tools on the ground", "hole", "pile of dirt"], # OK
+    "dog.jpg":                     ["dog", "grass"], 
 }
 
 
