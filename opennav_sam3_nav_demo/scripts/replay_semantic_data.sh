689796b5be7b0775c3baa4d9ad5f28aa541cadc6
Steve Macenski <stevenmacenski@gmail.com>
2026-05-26 04:07:28 -0700

    Clarify terminal usage in replay_semantic_data.sh
    Updated comments to clarify terminal usage for launching ROS2 nodes.

--- opennav_sam3_nav_demo/scripts/replay_semantic_data.sh
+++ opennav_sam3_nav_demo/scripts/replay_semantic_data.sh
@@ -5,7 +5,8 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source ~/amd_ws/install/setup.bash
 
-# ros2 launch opennav_sam3_inference sam3_inference.launch.py &
-# ros2 launch opennav_sam3_nav_demo nav2.launch.py &
+# Run these in other terminals
+# ros2 launch opennav_sam3_inference sam3_inference.launch.py
+# ros2 launch opennav_sam3_nav_demo nav2.launch.py
 
 ros2 bag play $1 -s mcap --clock
